20557e261f2be42f2ce27533e72a7632a923a749
Joe Kim <info@josephbkim.net>
2018-10-30 14:48:18 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,33 @@ console.log("Up and running!");
 
 var cards = ['queen', 'king', 'queen', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+//function that checks if both cards in play match
+var checkForMatch = function () {
+			if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 }
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+
+//flipcard function
+var flipCard = function (cardId) {
+
+	cardsInPlay.push(cards[cardId])
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
+	console.log("User flipped " + cards[cardId]);
+
+}
+
+
+//calls the function
+flipCard(0);
+flipCard(2);
