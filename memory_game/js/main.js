5fd8cbe950bbd3f9efbc86ef12ca0420eb1d27e6
Joe Kim <info@josephbkim.net>
2018-10-30 14:56:16 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and running!");
 
-var cards = ['queen', 'king', 'queen', 'king'];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"	
+}
+];
+
 var cardsInPlay = [];
 
 
@@ -11,22 +33,23 @@ var checkForMatch = function () {
 		} else {
 			alert("Sorry, try again.");
 		}
-}
+};
 
 
 
 //flipcard function
 var flipCard = function (cardId) {
 
-	cardsInPlay.push(cards[cardId])
+	cardsInPlay.push(cards[cardId].rank)
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 
-	console.log("User flipped " + cards[cardId]);
-
-}
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+};
 
 
 //calls the function
